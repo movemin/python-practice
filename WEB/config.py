4a859e41ad4376764ee8274a29ba440997c0d45f
movemin <easttalent12@g.yju.ac.kr>
2026-08-24 22:49:09 +0900

    답변 등록 라우트 함수 수정하기

--- WEB/config.py
+++ WEB/config.py
@@ -7,7 +7,6 @@ BASE_DIR = os.path.dirname(__file__)
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-
 SECRET_KEY = "dev"
 # ---오라클 버전---
 # # 1. Docker 가상 오라클 서버 접속 정보

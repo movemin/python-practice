33a738946258ad6ed9c15235223a454b3f851348
movemin <easttalent12@g.yju.ac.kr>
2026-08-24 22:41:31 +0900

    답변 등록 폼 추가하기

--- WEB/pybo/forms.py
+++ WEB/pybo/forms.py
@@ -3,5 +3,8 @@ from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired
 
 class QuestionForm(FlaskForm):
-    subject = StringField('제목', validators=[DataRequired()])
-    content = TextAreaField('내용', validators=[DataRequired()])
+    subject = StringField('제목', validators=[DataRequired('성수씨 제목 입력하세요')])
+    content = TextAreaField('내용', validators=[DataRequired('성수씨 내용도 빠뜨렸어요')])
+    
+class AnswerForm(FlaskForm):
+    content = TextAreaField('내용', validators=[DataRequired('한빈씨 답변해주세요.')])

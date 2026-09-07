03575ba81ab5b5b65919c219c09bf4dbe96254f4
movemin <easttalent12@g.yju.ac.kr>
2026-09-07 22:41:31 +0900

    question/question_list.html 파일의 질문 목록 출력 코드 수정하기

--- WEB/pybo/views/question_views.py
+++ WEB/pybo/views/question_views.py
@@ -9,15 +9,14 @@ bp = Blueprint('question', __name__, url_prefix='/question')
 @bp.route('/list/')
 def _list():
     page = request.args.get('page', type=int, default=1)  # 페이지
-    question_list = Question.query.order_by(Question.create_date.desc()).all()
-    question_list = question_list.paginate(page, per_page=10)
-    return render_template('/question/question_list.html', question_list=question_list)
+    question_list = Question.query.order_by(Question.create_date.desc()).paginate(page=page, per_page=10)
+    return render_template('question/question_list.html', question_list=question_list)
 
 @bp.route('/detail/<int:question_id>/')
 def detail(question_id):
     form = AnswerForm()
     question = Question.query.get_or_404(question_id)
-    return render_template('/question/question_detail.html', question = question, form=form)
+    return render_template('question/question_detail.html', question = question, form=form)
 
 @bp.route('/create/', methods=('GET', 'POST'))
 def create():

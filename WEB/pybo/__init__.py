af1bb3e386b28a7440ea20b3d269ffd528fa94c8
movemin <easttalent12@g.yju.ac.kr>
2026-09-08 21:57:01 +0900

    pybo/__init__.py 수정하여 필터 적용하기

--- WEB/pybo/__init__.py
+++ WEB/pybo/__init__.py
@@ -25,7 +25,9 @@ def create_app():
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
     app.register_blueprint(answer_views.bp)
+
+    from .filter import format_datetime
+    app.jinja_env.filters['datetime'] = format_datetime
     
-    from . import models # 작성 후: python -m flask --app pybo db migrate
     
     return app

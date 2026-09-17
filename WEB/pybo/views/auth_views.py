6911290f867d24e6877ead818eb2deb6858a42b9
movemin <easttalent12@g.yju.ac.kr>
2026-09-17 22:45:58 +0900

    내비게이션 바에서 회원가입 링크 추가

--- WEB/pybo/views/auth_views.py
+++ WEB/pybo/views/auth_views.py
@@ -12,7 +12,7 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 def signup():
     form = UserCreateForm()
     if request.method == 'POST' and form.validate_on_submit():
-        user = User.query.filter_by(uername=form.username.data).first()
+        user = User.query.filter_by(username=form.username.data).first()
         if not user:
             user = User(username=form.username.data,
                         password=generate_password_hash(form.password1.data),
@@ -20,6 +20,6 @@ def signup():
             db.session.add(user)
             db.session.commit()
             return redirect(url_for('main.index'))
-    else:
-        flash('이미 존재하는 사용자입니다.')
+        else:
+            flash('이미 존재하는 사용자입니다.')
     return render_template('auth/signup.html', form=form)

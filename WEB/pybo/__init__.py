ecb15ba6b9cfab4c97a8981ff21be4052c0de94c
movemin <easttalent12@g.yju.ac.kr>
2026-09-17 22:40:40 +0900

    template/form_errors.html 작성하기

--- WEB/pybo/__init__.py
+++ WEB/pybo/__init__.py
@@ -25,7 +25,7 @@ def create_app():
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
     app.register_blueprint(answer_views.bp)
-    app.register_blueprint(auth_views)
+    app.register_blueprint(auth_views.bp)
 
     from .filter import format_datetime
     app.jinja_env.filters['datetime'] = format_datetime
